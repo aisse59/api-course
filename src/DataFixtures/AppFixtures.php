cb041fdc682fb19192d3bde6b4fd2407b1e304ab
“aisse” <aisse.camara@gmail.com>
2023-04-30 09:11:19 +0200

    Ajout de l'entité User dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use DateTime;
 use DateTimeImmutable;
 
-use Faker\Factory;
+
 use App\Entity\Invoice;
 use App\Entity\Customer;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -21,28 +22,40 @@ class AppFixtures extends Fixture
         $endDate = new DateTime('now');
         $chrono = 1;
 
-        for ($c = 0; $c < 30; $c++) {
-            $customer = new Customer();
-            $customer->setFirstName($faker->firstName())
-                ->setLastName($faker->lastName)
-                ->setCompany($faker->company)
-                ->setEmail($faker->email);
+        for($u = 0; $u < 10; $u++) {
+            $user = new User();
+            $user->setFirstName($faker->firstName())
+                 -> setLastName($faker->lastName) 
+                 ->setEmail($faker->email)
+                 ->setPassword("password");
 
-            $manager->persist($customer);
+            $manager->persist($user);    
 
-            for ($i = 0; $i < mt_rand(3, 10); $i++) {
-                $invoice = new Invoice();
-                $invoice->setAmout($faker->randomFloat(2, 250, 5000))
-                    ->setSentAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween($startDate, $endDate)))
-                    ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
-                    ->setCustomer($customer)
-                    ->setChrono($chrono);
-
-                $chrono++;    
-
-                $manager->persist($invoice);
-            }
+            for ($c = 0; $c < mt_rand(5,20); $c++) {
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email);
+    
+                $manager->persist($customer);
+    
+                for ($i = 0; $i < mt_rand(3, 10); $i++) {
+                    $invoice = new Invoice();
+                    $invoice->setAmout($faker->randomFloat(2, 250, 5000))
+                        ->setSentAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween($startDate, $endDate)))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+                        ->setCustomer($customer)
+                        ->setChrono($chrono);
+    
+                    $chrono++;    
+    
+                    $manager->persist($invoice);
+                }
+            }    
         }
+
+        
 
         $manager->flush();
     }
